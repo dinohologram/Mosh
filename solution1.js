749d73158be4ac8e5778a04a43d0b86405378a05
John <johnmsaylor@gmail.com>
2020-01-13 13:57:34 -0500

    initial commit

---
+++ solution1.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb+srv://dino:gary@cluster0-c4ci4.mongodb.net/test?retryWrites=true&w=majority')
+  .then(() => console.log('Connected to DB'))
+  .catch(err => console.error('NOT connected -- Something Wrong', err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String, 
+  tags: [ String ],
+  date: Date, 
+  isPublished: Boolean,
+  price: Number
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function getCourses() {
+  return await Course
+  .find({ isPublished: true, tags: 'backend' })
+  .sort({ name: 1 })
+  .select({ name: 1, author: 1 })
+  .catch(err => console.error("ahhh", err));  
+}
+
+async function run() {
+  let courses = await getCourses()
+  console.log(courses)
+}
+
+run()
